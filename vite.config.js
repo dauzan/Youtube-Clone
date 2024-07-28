2368a9b5fa537b4431868e153c9c948a47164b48
Dzaldy <dzaldysaputro@gmail.com>
2024-07-29 00:01:16 +0700

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    }
+  },
   plugins: [react()],
 })
